9265d91a432bd296f3f377e8bb5c96f3861188e5
YufeizhangRay <yufeizhangray@gmail.com>
2019-01-03 16:23:55 -0500

    RabbitMQ 原理解析

---
+++ rabbitmq-javaapi/src/main/java/cn/zyf/util/ResourceUtil.java
@@ -0,0 +1,16 @@
+package cn.zyf.util;
+
+import java.util.ResourceBundle;
+
+public class ResourceUtil {
+    private static final ResourceBundle resourceBundle;
+
+    static{
+        resourceBundle = ResourceBundle.getBundle("config");
+    }
+
+    public static String getKey(String key){
+        return resourceBundle.getString(key);
+    }
+
+}

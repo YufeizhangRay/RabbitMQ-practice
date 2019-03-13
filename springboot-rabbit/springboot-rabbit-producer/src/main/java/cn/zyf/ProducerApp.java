6c1e7e9d66df7321dce1eea05c4dcfaca184c59b
Yufei Zhang <42585485+YufeizhangRay@users.noreply.github.com>
2019-03-13 00:42:06 -0400

    RabbitMQ 原理解析

--- springboot-rabbit/springboot-rabbit-producer/src/main/java/cn/zyf/ProducerApp.java
+++ springboot-rabbit/springboot-rabbit-producer/src/main/java/cn/zyf/ProducerApp.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-@MapperScan("com.gupaoedu.mapper")
+@MapperScan("cn.zyf.mapper")
 public class ProducerApp {
 
 	public static void main(String[] args) {
